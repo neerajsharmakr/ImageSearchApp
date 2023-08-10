58081c78e2e7f5af5204b095dbc6342e77b30808
Neeraj Kumar Sharma <neerajsharmakr>
2023-08-11 00:46:27 +0530

    update files

---
+++ index.js
@@ -0,0 +1,54 @@
+const accessKey = "L6c6q6jS1R85a92bAfPQ0Q6dkVuJDgBttzh5nXfDcOQ";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchresults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+
+  if (page === 1) {
+    searchresults.innerHTML = "";
+  }
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchresults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click", () => {
+  searchImages();
+});
